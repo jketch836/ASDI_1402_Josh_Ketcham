88b6c05c70a098795299e70ea4f2a6b9f88271c5
jketch836 <jketch@JoshKs-MacBook-Pro.local>
2014-02-26 20:52:58 -0500

    ASDI_LAB4_ P3
    asdi.lab4 crud api

--- asdi.lab4/Resources/app.js
+++ asdi.lab4/Resources/app.js
@@ -1,49 +1,22 @@
-var mWin = Ti.UI.createWindow({
-	title : 'Council of Judgement',
-	backgroundColor : '#fff'
+var table = require('RemoteTableView');
+var memSave = require('favorites');
+
+// created tab group
+var theTabs = Ti.UI.createTabGroup();
+
+// created character tab
+var toonTab = Ti.UI.createTab({
+	title : 'Council Members',
+	window : table.tableWin
 });
 
-var mainWinNav = Ti.UI.iOS.createNavigationWindow({
-	window : mWin
+// created favorite tab
+var favTab = Ti.UI.createTab({
+	title : 'Favorite Members',
+	window : memSave.fWin
 });
-
-var picTabard = Ti.UI.createImageView({
-	height : Ti.UI.setHeight,
-	width : Ti.UI.setWidth,
-	image : 'guild_tabard.png'
-});
-
-//ButtonBar start
-var bars = Titanium.UI.createButtonBar({
-	labels : ['Council Members', 'Favorite Members'],
-	backgroundColor : '#000080',
-	top : 10,
-	style : Titanium.UI.iPhone.SystemButtonStyle.BAR,
-	height : 40,
-	width : 250
-});
-
-bars.addEventListener('click', function(toons) {
-	if (toons.index === 0) {
-		var toonWin = Ti.UI.createWindow({
-			title : 'CoJ Members',
-			backgroundColor : '#fff',
-			url : 'remoteAPI.js'
-		});
-	mainWinNav.openWindow(toonWin, {animate : true});
-	
-	} else if (toons.index === 1) {
-		var favWin = Ti.UI.createWindow({
-			title : 'Favorite Members',
-			backgroundColor : '#fff',
-			url : 'favorites.js',
-			nav : mainWinNav
-		});
-		mainWinNav.openWindow(favWin, {animate : true});
-	};
-});
-//ButtonBar End
 
 //Main Code
-mWin.add(picTabard, bars);
-mainWinNav.open();
+theTabs.addTab(toonTab);
+theTabs.addTab(favTab);
+theTabs.open();
--- asdi.lab4/Resources/favorites.js
+++ asdi.lab4/Resources/favorites.js
@@ -1,20 +1,19 @@
 var crud = require('CrudFile');
 
 var fWin = Ti.UI.createWindow({
-	top : 65,
-	height : Ti.UI.setHeight,
-	width : Ti.UI.setWidth
+	title : 'Favorite Members',
+	backgroundColor : '#fff'
 });
+exports.fWin = fWin;
 
-var picTabard = Ti.UI.createImageView({
-	height : Ti.UI.setHeight,
-	width : Ti.UI.setWidth,
-	image : 'guild_tabard.png'
+var searchbar = Ti.UI.createSearchBar({
+	top : 0,
+	hintText : 'Search',
+	barColor : '#fff'
 });
 
 var saveMem = Titanium.UI.createTableView({
-	opacity : .8,
-	// text : level,
+	search : searchbar,
 	font : {
 		fontStyle : 'Helvetica',
 		fontSize : '12dp'
@@ -22,82 +21,10 @@ var saveMem = Titanium.UI.createTableView({
 });
 
 saveMem.addEventListener('click', function() {
-	var savedWin = Ti.UI.createWindow({
-		// title : name,
-		backgroundColor : '#fff'
-	});
-
-	var saveNav = Ti.UI.iOS.createNavigationWindow({
-		window : savedWin
-	});
-
-	var cancel = Ti.UI.createButton({
-		systemButton : Ti.UI.iPhone.SystemButton.CANCEL
-	});
-	savedWin.setLeftNavButton(cancel);
-
-	cancel.addEventListener('click', function() {
-		saveNav.close();
-		savedWin.close();
-	});
-	
-	// var nameView = Ti.UI.createLabel({
-		// top : '75dp',
-		// left : 35,
-		// text : 'Name: ' + name
-	// });
-// 
-	// var lvlView = Ti.UI.createLabel({
-		// top : '175dp',
-		// left : 35,
-		// text : 'Level: ' + level
-	// });
-// 
-	// var achView = Ti.UI.createLabel({
-		// top : '125dp',
-		// left : 35,
-		// text : 'Acheivment Points: ' + acheive
-	// });
-// 
-	// var divView = Ti.UI.createLabel({
-		// top : '225dp',
-		// left : 35,
-		// text : 'Class: ' + division
-	// });
-
-	var editBTN = Ti.UI.createButton({
-		systemButton : Ti.UI.iPhone.SystemButton.EDIT
-	});
-	// editBTN.addEventListener('click', crud.edit);
-	editBTN.addEventListener('click', editFunc);
-
-	savedWin.setRightNavButton(editBTN);
-	// savedWin.add(nameView, lvlView, achView, divView);
-	saveNav.open();
+	// crud.editWin();
 });
 
 // Main Code
 saveMem.setData(crud.guildData);
-fWin.add(picTabard, saveMem);
-fWin.open();
-
-
-function editFunc () {
-	var editDIA = Ti.UI.createAlertDialog({
-		title : 'Edit/Delete',
-		buttonNames : ['Edit', 'Delete', 'Cancel']
-	});
-	editDIA.show();
-	editDIA.addEventListener('click', function(b) {
-		if (b.index === 0) {
-			crud.data.execute('UPDATE council SET name, division, level, acheive WHERE id=?', name, division, level, acheive,id);
-		} else if (b.index === 1) {
-			crud.data.execute('DELETE FROM council WHERE id=?', id);
-			crud.guildData;
-			saveNav.close();
-			savedWin.close();
-			alert(name + ' is Deleted');
-		} else {null;
-		}
-	});
-};
+fWin.add(saveMem);
+fWin.open(); 

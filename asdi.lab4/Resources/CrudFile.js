23f8b2b264664d3595ad426daf58ff1034a4848b
jketch836 <jketch@JoshKs-MacBook-Pro.local>
2014-02-28 12:35:47 -0500

    asdi.lab4 p4
    asdi.lab4 p4

--- asdi.lab4/Resources/CrudFile.js
+++ asdi.lab4/Resources/CrudFile.js
@@ -1,8 +1,8 @@
 var data = Ti.Database.open('council');
 var wowData = Ti.App.Properties.getString('wowData');
-data.execute('CREATE TABLE IF NOT EXISTS council (id INTEGER PRIMARY KEY, name TEXT, division TEXT, level TEXT, acheive TEXT)');
+data.execute('CREATE TABLE IF NOT EXISTS council (id INTEGER PRIMARY KEY, name TEXT, division TEXT, level TEXT, acheive TEXT, comment TEXT)');
 
-
+//inserting Data function begin
 function insertData() {
 	var dataInfo = [];
 
@@ -13,6 +13,7 @@ function insertData() {
 		var division = rowInfo.fieldByName('division');
 		var level = rowInfo.fieldByName('level');
 		var acheive = rowInfo.fieldByName('acheive');
+		var comment = rowInfo.fieldByName('comment');
 		var id = rowInfo.fieldByName('id');
 
 		dataInfo.push({
@@ -20,13 +21,10 @@ function insertData() {
 			level : level,
 			acheive : acheive,
 			division : division,
+			comment : comment,
 			title : name + "      " + division + "      " + level,
 			id : id
 		});
-
-		Ti.API.info('End row ' + id + ' look');
-
-		Ti.API.info('=========================');
 
 		rowInfo.next();
 	};
@@ -34,131 +32,57 @@ function insertData() {
 };
 var guildData = insertData();
 exports.guildData = guildData;
+// console.log(guildData);
+//inserting Data function end
 
-console.log(guildData);
-
-
-var saveFunc = function(name, division, level, acheive) {
+//save function begin
+var saveFunc = function(name, division, level, acheive, comment) {
+	
 	input = {};
 	input.gname = name;
 	input.div = division;
 	input.lvl = level;
 	input.ach = acheive;
-	data.execute('INSERT INTO council (name, division, level, acheive) VALUES (?, ?, ?, ?)', input.gname, input.div, input.lvl, input.ach);
+	input.com = comment;
+	//pushing to guildData
+	data.execute('INSERT INTO council (name, division, level, acheive, comment) VALUES (?, ?, ?, ?, ?)', input.gname, input.div, input.lvl, input.ach, input.com);
 };
 exports.saveFunc = saveFunc;
+//save function end
 
-
-exports.select = function(a) {
+var select = function(a) {
 	var id = a.rowData.id;
 	input = {};
 	input.gname = name;
 	input.div = division;
 	input.lvl = level;
 	input.ach = acheive;
+	input.com = comment;
 	input.id = id;
 	data.execute('SELECT * FROM council WHERE ID=?', id);
 };
 
+// //edit function begin
+// function editFunc(a) {
+// var id = a.rowData.id;
+// input = {};
+// input.gname = name;
+// input.div = division;
+// input.lvl = level;
+// input.ach = acheive;
+// input.com = comment;
+// input.id = id;
+// data.execute('UPDATE council SET name, division, level, acheive WHERE id=?', input.gname, input.div, input.lvl, input.ach, input.id);
+// guildData;
+// };
+// exports.editFunc = editFunc();
+// //edit function end
 
-function editFunc() {
-	input = {};
-	input.gname = name;
-	input.div = division;
-	input.lvl = level;
-	input.ach = acheive;
-	input.id = id;
-	data.execute('UPDATE council SET name, division, level, acheive WHERE id=?', input.gname, input.div, input.lvl, input.ach, input.id); guildData;
+//delete function begin
+function delFunc(a) {
+	// var id = a.rowData.id;
+	// data.execute('DELETE FROM council WHERE id=?', id); 
+	guildData;
 };
-exports.edit = editFunc;
-
-
-function delFunc() {
-	data.execute('DELETE FROM council WHERE id=?', id); guildData;
-};
-exports.del = delFunc;
-
-
-exports.editWin = function() {
-
-	input = {};
-	input.gname = name;
-	input.div = division;
-	input.lvl = level;
-	input.ach = acheive;
-
-	var memWin = Ti.UI.createWindow({
-		title : input.gname,
-		backgroundColor : '#fff'
-	});
-
-	var memWinNav = Ti.UI.iOS.createNavigationWindow({
-		window : memWin
-	});
-
-	var cancelBTN = Ti.UI.createButton({
-		systemButton : Ti.UI.iPhone.SystemButton.CANCEL
-	});
-
-	cancelBTN.addEventListener('click', function() {
-		memWinNav.close();
-		memWin.close();
-	});
-
-	var nameView = Ti.UI.createLabel({
-		top : '75dp',
-		left : 35,
-		text : 'Name: ' + input.gname
-	});
-
-	var lvlView = Ti.UI.createLabel({
-		top : '175dp',
-		left : 35,
-		text : 'Level: ' + input.lvl
-	});
-
-	var achView = Ti.UI.createLabel({
-		top : '125dp',
-		left : 35,
-		text : 'Acheivment Points: ' + input.ach
-	});
-
-	var divView = Ti.UI.createLabel({
-		top : '225dp',
-		left : 35,
-		text : 'Class: ' + input.div
-	});
-	var saveBTN = Ti.UI.createButton({
-		systemButton : Ti.UI.iPhone.SystemButton.SAVE
-	});
-	saveBTN.addEventListener('click', function() {
-		saveFunc();
-		memWin.close();
-		memWinNav.close();
-		alert(name + " is saved");
-	});
-
-	var deleteBTN = Ti.UI.createButton({
-		top : 290,
-		center : 0,
-		title : "Delete",
-		style : Ti.UI.iPhone.SystemButtonStyle.ACTION,
-		font : {
-			fontWeight : 'bold',
-			fontSize : 18,
-			fontFamily : 'Helvetica'
-		},
-		color : '#EE2C2C'
-	});
-	deleteBTN.addEventListener('click', function() {
-		delFunc();
-		memWin.close();
-		memWinNav.close();
-		alert(name + " is deleted");
-	});
-
-	memWin.setRightNavButton(saveBTN);
-	memWin.setLeftNavButton(cancelBTN);
-	memWin.add(nameView, lvlView, achView, divView);
-	memWinNav.open();
-};
+exports.delFunc = delFunc();
+//delete function end
